ef7ae007bd1bbdbbfbad780c34fe53dc00f1b6ed
_antonio_rosa <64078733+AntonioMGRosa@users.noreply.github.com>
2021-01-16 20:13:52 +0000

    layout recyclerView/itemList da Tarefa People
    Layout da RecyclerView feita, assim como do item list associado à mesma da tarefa People

--- app/src/main/java/com/example/a17179_lauramelissa_17183_antoniorosa_tp_pdm_2019_2020/PeopleActivity.java
+++ app/src/main/java/com/example/a17179_lauramelissa_17183_antoniorosa_tp_pdm_2019_2020/PeopleActivity.java
@@ -1,18 +1,15 @@
 package com.example.a17179_lauramelissa_17183_antoniorosa_tp_pdm_2019_2020;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class PeopleActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,16 +19,14 @@ public class PeopleActivity extends AppCompatActivity {
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_menu);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-        this.toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.people);
     }
 
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-        new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            item -> {
                 Intent resultIntent = null;
                 switch (item.getItemId()) {
                     case R.id.nav_tasks:
@@ -44,6 +39,5 @@ public class PeopleActivity extends AppCompatActivity {
                 startActivity(resultIntent);
                 PeopleActivity.this.finish();
                 return true;
-            }
-        };
+            };
 }
