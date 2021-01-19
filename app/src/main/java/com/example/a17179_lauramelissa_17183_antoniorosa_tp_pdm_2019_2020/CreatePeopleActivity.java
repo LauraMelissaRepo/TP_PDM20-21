470cb78b0ef3219604238bbfc5fbbc360a2ed730
_antonio_rosa <64078733+AntonioMGRosa@users.noreply.github.com>
2021-01-19 11:54:36 +0000

    Guardar path de uma foto tirada e guardada. Guardar path de uma foto selecionada da galeria

--- app/src/main/java/com/example/a17179_lauramelissa_17183_antoniorosa_tp_pdm_2019_2020/CreatePeopleActivity.java
+++ app/src/main/java/com/example/a17179_lauramelissa_17183_antoniorosa_tp_pdm_2019_2020/CreatePeopleActivity.java
@@ -4,16 +4,13 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
-import android.Manifest;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -26,7 +23,6 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 
 public class CreatePeopleActivity extends AppCompatActivity {
 
@@ -36,6 +32,7 @@ public class CreatePeopleActivity extends AppCompatActivity {
     public static final int CAMERA_REQUEST_CODE = 2000;
     public static final int GALERY_REQUEST_CODE = 1000;
     private String currentPhotoPath;
+    private File photoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,23 +46,9 @@ public class CreatePeopleActivity extends AppCompatActivity {
         Button takePictureButton = findViewById(R.id.takePicture);
         takePictureButton.setOnClickListener(v -> {
             Intent intentTakePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (intentTakePicture.resolveActivity(getPackageManager()) != null){
-                File photoFile = null;
-                try{
-                    photoFile = createImageFile();
-                }catch (Exception exception){
-                    //Error ocurred while creating the File
-                }
-                //Continue only if the File was sucessfully created
-                if(photoFile != null){
-                    Uri photoUri = FileProvider.getUriForFile(this,
-                            "com.example.a17179_lauramelissa_17183_antoniorosa_tp_pdm_2019_2020.fileprovider",
-                            photoFile);
-                    intentTakePicture.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-                    startActivityForResult(intentTakePicture, CAMERA_REQUEST_CODE);
-                    galleryAddPic();
-                }
-            }
+            Uri photoUri = createFile();
+            intentTakePicture.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            startActivityForResult(intentTakePicture, CAMERA_REQUEST_CODE);
         });
 
         Button selectPictureButton = findViewById(R.id.selectPicture);
@@ -89,36 +72,63 @@ public class CreatePeopleActivity extends AppCompatActivity {
                     .addOnSuccessListener(this, documentReference -> {
                         Toast.makeText(getApplicationContext(), "Chegou", Toast.LENGTH_LONG).show();
                     });
+            galleryAddPic();
             finish();
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == CAMERA_REQUEST_CODE){
-            if(resultCode == RESULT_OK){
-                Bitmap bitmap = data.getParcelableExtra("data");
-                this.imageView.setImageBitmap(bitmap);
-            }
-            else {
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Uri uri = Uri.fromFile(this.photoFile);
+                Bitmap bitmap;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    this.imageView.setImageBitmap(bitmap);
+                    Toast.makeText(this, this.currentPhotoPath, Toast.LENGTH_LONG).show();
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+                }
+            } else {
                 Toast.makeText(this, "404 Pic Not Found", Toast.LENGTH_LONG).show();
             }
-        }
-        else if (requestCode == GALERY_REQUEST_CODE){
-            if(resultCode == RESULT_OK){
+        } else if (requestCode == GALERY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
                 Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                this.currentPhotoPath = cursor.getString(columnIndex);
                 this.imageView.setImageURI(selectedImage);
-            }
-            else {
+                Toast.makeText(this, this.currentPhotoPath, Toast.LENGTH_LONG).show();
+            } else {
                 Toast.makeText(this, "404 Pic Not Found", Toast.LENGTH_LONG).show();
             }
-        }
-        else{
+        } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
-    private File createImageFile() throws Exception{
+    private Uri createFile() {
+        this.photoFile = null;
+        try {
+            this.photoFile = createImageFile();
+        } catch (Exception exception) {
+            //Error ocurred while creating the File
+        }
+        //Continue only if the File was sucessfully created
+        if (this.photoFile != null) {
+            Uri photoUri = FileProvider.getUriForFile(this,
+                    "com.example.a17179_lauramelissa_17183_antoniorosa_tp_pdm_2019_2020.fileprovider",
+                    this.photoFile);
+            return photoUri;
+        }
+        return null;
+    }
+
+    private File createImageFile() throws Exception {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
@@ -133,7 +143,7 @@ public class CreatePeopleActivity extends AppCompatActivity {
         return image;
     }
 
-    private void galleryAddPic(){
+    private void galleryAddPic() {
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         File f = new File(this.currentPhotoPath);
         Uri contentUri = Uri.fromFile(f);
